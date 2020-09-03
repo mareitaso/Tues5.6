c424dd15d9e82469eaf17f0f1ee3e2344b4274da
MASATO MINAGAWA <kagepuro.mekakusi@icloud.com>
2020-09-03 15:56:44 +0900

    エフェクトを制作しました。

---
+++ RunGame/Assets/Member/Minagawa/Scripts/EffectBreaker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectBreaker : MonoBehaviour
+{
+    void Start()
+    {
+        //エフェクトが生成されて1秒後にオブジェクトを削除する
+        Invoke("BreakEffect", 1.0f);
+    }
+
+    //エフェクト(自分自身)を削除する
+    void BreakEffect()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
