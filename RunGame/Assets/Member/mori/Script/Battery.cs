ff437938bda80e88e80e714341da3b28135f6c8e
arufa2530 <moriken.0325@gmail.com>
2020-09-08 18:26:33 +0900

    砲台を作成 / 砲台のプレハブを作成　

--- RunGame/Assets/Member/mori/Script/Battery.cs
+++ RunGame/Assets/Member/mori/Script/Battery.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Battery : MonoBehaviour
 {
+    [SerializeField]
+    GameObject BatteryBullet;
+    float _time = 2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class Battery : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _time -= Time.deltaTime;
+        if(_time <= 0)
+        {
+            Instantiate(BatteryBullet, transform.position, Quaternion.Euler(0,0,180f));
+            _time = 2; 
+        }
     }
 }
