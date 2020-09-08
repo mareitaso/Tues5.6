fcb6e869aaa6f65ff50ba45c9b99c823b1b9be71
Tomioka <17c1020019tt@edu.tech.ac.jp>
2020-09-08 12:03:21 +0900

    統合中

---
+++ RunGame/Assets/Member/Tomioka/Scripts/BombTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombTest : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bombEffect;
+
+    private GameObject bomb;
+
+    void Start()
+    {
+        UseBomb(new Vector2(700, 300));
+    }
+    public void UseBomb(Vector2 direction)
+    {
+        GetComponent<Rigidbody2D>().AddForce(direction);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == ("wall"))
+        {
+            Destroy(this.gameObject);
+        }
+        if (col.gameObject.tag == ("Stage"))
+        {
+            Invoke("OldBomb", 0.5f);
+        }
+    }
+
+    private void OldBomb()
+    {
+        bomb = Instantiate(bombEffect, new Vector2(this.transform.position.x, this.transform.position.y), Quaternion.identity);
+        this.gameObject.SetActive(false);
+        Invoke("DestroyBomb", 0.5f);
+    }
+
+    private void DestroyBomb()
+    {
+        Destroy(bomb);
+        Destroy(this.gameObject);
+    }
+}
---
+++ RunGame/Assets/Member/Tomioka/Scripts/Stage.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+    private const int StageTipSize = 30;
+
+    private int currentTipIndex;
+
+    [SerializeField]
+    private Transform character;
+
+    [SerializeField]
+    private GameObject[] stageTips;
+
+    [SerializeField]
+    private int startTipIndex;
+
+    [SerializeField]
+    private int preInstantiate;
+
+    [SerializeField]
+    private List<GameObject> generatedStageList = new List<GameObject>();
+
+    void Start()
+    {
+        currentTipIndex = startTipIndex - 1;
+        UpdateStage(preInstantiate);
+    }
+
+
+    void Update()
+    {
+        int charaPositionIndex = (int)(character.position.x / StageTipSize);
+        if (charaPositionIndex + preInstantiate > currentTipIndex)
+        {
+            UpdateStage(charaPositionIndex + preInstantiate);
+        }
+
+    }
+    //指定の場所までのステージを生成
+    private void UpdateStage(int toTipIndex)
+    {
+        if (toTipIndex <= currentTipIndex) return;
+
+        for (int i = currentTipIndex + 1; i <= toTipIndex; i++)
+        {
+            GameObject stageObject = GenerateStage(i);
+
+            generatedStageList.Add(stageObject);
+        }
+        
+        while (generatedStageList.Count > preInstantiate + 2) DestroyOldestStage();
+
+        currentTipIndex = toTipIndex;
+    }
+
+    //ステージの生成
+    private GameObject GenerateStage(int tipIndex)
+    {
+        int nextStageTip = Random.Range(0, stageTips.Length);
+
+        GameObject stageObject = (GameObject)Instantiate(
+            stageTips[nextStageTip],
+            new Vector3(tipIndex * StageTipSize, 0, 0),
+            Quaternion.identity);
+        return stageObject;
+    }
+
+    //一番古いステージを消す
+    private void DestroyOldestStage()
+    {
+        GameObject oldStage = generatedStageList[0];
+        generatedStageList.RemoveAt(0);
+        Destroy(oldStage);
+    }
+
+}
