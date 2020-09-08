779b6eb95bbc175ead61cf572e7e5bb852b41d0a
MASATO MINAGAWA <kagepuro.mekakusi@icloud.com>
2020-09-08 18:27:59 +0900

    TitelとResultのシーンを制作、指定のシーンに移動できるよう制御済み

---
+++ RunGame/Assets/Member/Minagawa/Scripts/TitleDirector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class TitleDirector : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+}
